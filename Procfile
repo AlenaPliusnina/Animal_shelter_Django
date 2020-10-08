5477ecc6ae7ef8be976108ac5d4c8d7c46cb276e
AlenaPliusnina <a564352@gmail.com>
2020-10-08 11:23:31 +0300

    add new database

--- Procfile
+++
@@ -1,3 +0,0 @@
-release: python manage.py migrate
-release: python manage.py loaddata fixtures.json
-web: gunicorn my_site.wsgi
--- my_site/settings.py
+++ my_site/settings.py
@@ -80,19 +80,25 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_animal_shelter',
-        'USER': 'postgres',
-        'PASSWORD': 'Q1w2e3r4',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_animal_shelter',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Q1w2e3r4',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # import dj_database_url
 #
 # DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
