343285d19eaf4a6c0f0879e2232e76e645d6141e
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2020-12-10 19:41:58 +0300

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&eubb)%sdg^w3p2sa5ica3w==81yi*b%e@+u%v0)jg(wu(xu4c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ## ----- Для Heroku ----- ##
 # SECRET_KEY = os.environ.get('SECRET_KEY')
